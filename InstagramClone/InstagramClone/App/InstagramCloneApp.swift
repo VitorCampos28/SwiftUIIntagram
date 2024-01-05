bcc9c7d2d4dc966e7f7c3f2a5146eb427b61807d
Vitor Campos <vitor.natal@timwe.com>
2024-01-05 13:52:24 -0300

    Tabbar created

---
+++ InstagramClone/InstagramClone/App/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Vitor Campos on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
